b78d7d86d0ddb7712ffbf058359407563191f832
tomislav-ficko <ficko.tomislav@gmail.com>
2020-10-23 22:22:33 +0200

    Minor refactor

--- radionice/r1_zadatak7.erl
+++ radionice/r1_zadatak7.erl
@@ -7,9 +7,9 @@
 -module(r1_zadatak7).
 -author("Tomi").
 
--export([run/0, input_loop/1, find_element/1]).
+-export([run/0, input_loop/1, find_element/1, get_key/0, get_value/0]).
 -import(maps, [put/3]).
--import(io, [get_line/1, format/2]).
+-import(io, [get_line/1, format/1, format/2]).
 
 % Zadatak: Potražiti u Erlangovoj dokumentaciji gotove module koji predstavljaju mape
 %           - odlučiti se za jedan i koristiti ga u funkciji iz zadatka 6
@@ -20,28 +20,37 @@ run() ->
     find_element(Map).
 
 input_loop(Map) ->
-    KeyInput = get_line("Please enter a key (to finish write exit): "),
-    Key = string:strip(KeyInput, right, $\n),
+    Key = get_key(),
     if
         Key == "exit" ->
             % Vrati se u pozivajuću funkciju s popunjenom mapom
             Map;
         true ->
-            ValueInput = get_line("Please enter a value: "),
-            Value = string:strip(ValueInput, right, $\n),
-
+            Value = get_value(),
             NewMap = maps:put(Key, Value, Map),
             input_loop(NewMap)
     end.
 
 find_element(Map) ->
-    Input = get_line("Please enter a key to search for: "),
-    Key = string:strip(Input, right, $\n),
-    Tuple = maps:find(Key, Map),
+    Key = get_key(),
     if
-        Tuple == error ->
-            format("-> There is no value associated with key ~p.~n", [Key]);
+        Key == "exit" ->
+            format("Search terminated.");
         true ->
-            {ok, Value} = Tuple,
-            format("-> The value for ~p is ~p.~n", [Key, Value])
+            Tuple = maps:find(Key, Map),
+            if
+                Tuple == error ->
+                    format("-> There is no value associated with key ~p.~n", [Key]);
+                true ->
+                    {ok, Value} = Tuple,
+                    format("-> The value for ~p is ~p.~n", [Key, Value])
+            end
     end.
+
+get_key() ->
+    Input = get_line("Please enter a key (to finish write exit): "),
+    string:strip(Input, right, $\n).
+
+get_value() ->
+    Input = get_line("Please enter a value: "),
+    string:strip(Input, right, $\n).
