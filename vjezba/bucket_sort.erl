0afb443bff2a82163f59f3185b20ab9d79b4ce4a
tomislav-ficko <ficko.tomislav@gmail.com>
2020-10-26 10:23:47 +0100

    Add exercise for the bucket sort algorithm (which internally uses bubble sort)

---
+++ vjezba/bucket_sort.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author Tomi
+%%% @copyright (C) 2020, Ficko
+%%% 
+%%% Created : Oct 26, 2020 10:31
+%%%-------------------------------------------------------------------
+-module(red_black_tree).
+-author("Tomi").
+
+-export([run/0, bucket_sort/1]).
+-import(io, [get_line/1, format/1, format/2]).
+
+% Task: Create implementation of the bucket sort algorithm, using bubble sort.
+%
+% Implement function 'bucket_sort(List)' which sorts the list of input data.
+%
+% Requirements:
+%           -> Values inside the list will be whole numbers.
+%           -> The implementation needs to be done within a single process.
+
+
+run() ->
+    
+    todo.
+
+bucket_sort(List) ->
+
+    todo.
