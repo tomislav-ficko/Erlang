50cdd0eb057b1d51fa1b6103caa1dd865b156eef
tomislav-ficko <ficko.tomislav@gmail.com>
2020-10-30 22:11:46 +0100

    Complete task 4 (add function for retrieving friends of friends)

--- radionice/r2_zadatak.erl
+++ radionice/r2_zadatak.erl
@@ -7,7 +7,7 @@
 -module(r2_zadatak).
 -author("Tomi").
 
--export([run/0, process_loop/1, balancer/0, index_of/2]).
+-export([run/0, process_loop/1, balancer/1, index_of/2]).
 -import(io, [get_line/2, format/1, format/2]).
 -import(r1_zadatak7, [find_element/1]).
 -import(string, [uppercase/1]).
@@ -16,8 +16,14 @@
 % .. mape (ključ n-torka ime i prezime, vrijednost je lista prijatelja - n-torka ime i prezime).
 % Zadatak 2: Napraviti sekvencijski API za proces.
 % Zadatak 3: Raspodijeliti odgovornost na dva procesa koji svaki obrađuje dio ključeva i glavni proces koji zna preusmjeriti zahtjeve.
+% Zadatak 4: Napraviti funkciju koja prima osobu kao ključ i vraća listu prijatelja s određenim brojem skokova 
+% .. npr. za broj skokova 2 nađe i prijatelje od prijatelja. U listi se ne smije nalaziti više puta ista osoba.
 
 run() ->
+    %test1().
+    test2().
+
+test1() ->
     start_balancer(),
     
     Key = {"Michael", "Schumacher"},
@@ -38,6 +44,23 @@ run() ->
     stop_balancer(),
     ok.
 
+test2() ->
+    start_balancer(),
+
+    K1 = {"Marcus", "Wagner"},
+    V1 = [{"Mimi", "Hauser"}, {"Betty", "Boop"}],
+    map_insert(K1, V1),
+    K2 = {"Mimi", "Hauser"},
+    V2 = [{"Tony","Stark"}, {"Planty", "Pop"}],
+    map_insert(K2, V2),
+
+    Depth = 2,
+    FriendsOfFriends = map_get_related(K1, Depth),
+    format("Related friends of ~p at depth ~p are:~n", [K1, Depth]),
+    format(" ~p.~n", [FriendsOfFriends]),
+
+    stop_balancer,
+    ok.
 
 map_insert(Key, Value) ->
     balancer!{self(), insert, Key, Value}.
@@ -49,43 +72,65 @@ map_get(Key) ->
     balancer!{self(), get, Key, no_value},
     receive
         Result ->
-            Result
+            if
+                Result == key_not_present ->
+                    [];
+                true ->
+                    Result
+            end
     end.
 
 map_delete(Key) ->
     balancer!{self(), delete, Key, no_value}.
 
+map_get_related(Key, Depth) ->
+    get_related([], map_get(Key), [], Depth).
+
+get_related([], Remaining, _, 1) ->
+    % Easiest case, when the Depth argument in map_get_related() is one. Only the immediate friends need to be retrieved
+    Remaining;
+get_related(Seen, _, _, 0) ->
+    % Case when we've come to the end of the recursion
+    Seen;
+get_related(Seen, [Current | Remaining], RemainingOnNextDepth, Depth) ->
+    % Case when we have to go deeper into the recursion
+    ContainedInsideSeen = lists:member(Current, Seen),
+    if
+        ContainedInsideSeen == true ->
+            get_related(Seen, Remaining, RemainingOnNextDepth, Depth);
+        true ->
+            NewSeen = Seen ++ [Current],
+            NewRemainingOnNextDepth = RemainingOnNextDepth ++ map_get(Current),
+            get_related(NewSeen, Remaining, NewRemainingOnNextDepth, Depth)
+    end;
+get_related(Seen, [], RemainingOnNextDepth, Depth) ->
+    % Case when we don't have anyone left to search for, on the current depth
+    get_related(Seen, RemainingOnNextDepth, [], Depth - 1).
 
 start_balancer() ->
-    Pid = spawn(module_info(module), balancer, []),
-    register(balancer, Pid),
-
     EmptyMap = maps:new(),
     Pid1 = spawn(module_info(module), process_loop, [EmptyMap]),
-    register(worker1, Pid1),
     Pid2 = spawn(module_info(module), process_loop, [EmptyMap]),
-    register(worker2, Pid2).
+    % If adding a process, edit here
+    ProcessList = [Pid1, Pid2],
+
+    Pid = spawn(module_info(module), balancer, [ProcessList]),
+    register(balancer, Pid).
+
 
 stop_balancer() ->
-    balancer!stop,
-    worker1!stop,
-    worker2!stop.
+    balancer!stop.
 
-balancer() ->
+balancer(ProcessList) ->
     receive
         {From, Action, Key, Value} ->
 
             {_, Surname} = Key,
             [FirstLetter | _] = Surname,
-            % Funkcija služi za određivanje indexa (X u "workerX") procesa koji će obraditi zahtjev
-            WorkerIndex = get_index_of_worker(uppercase([FirstLetter])),
- 
-            if
-                WorkerIndex == 1 ->
-                    Pid = whereis(worker1);
-                WorkerIndex == 2 ->
-                    Pid = whereis(worker2)
-            end,
+            % Funkcija služi za određivanje indexa procesa koji će obraditi zahtjev
+            WorkerIndex = get_index_of_worker(uppercase([FirstLetter]), length(ProcessList)),
+
+            Pid = lists:nth(WorkerIndex, ProcessList),
             
             if
                 Action == insert ->
@@ -94,18 +139,18 @@ balancer() ->
                     api_update(Pid, Key, Value);
                 Action == get ->
                     Result = api_get(Pid, Key),
-                    From!{Result};
+                    From!Result;
                 Action == delete ->
                     api_delete(Pid, Key)
             end,
-            balancer();
+            balancer(ProcessList);
         stop ->
+            lists:foreach(fun(Pid) -> Pid!stop end, ProcessList),
             true
     end.
 
-get_index_of_worker(UppercaseLetter) ->
+get_index_of_worker(UppercaseLetter, NumberOfProcesses) ->
     AlphabetLength = length(uppercase_alphabet()),
-    NumberOfProcesses = 2,
 
     PositionInAlphabet = index_of(UppercaseLetter, uppercase_alphabet()),
     if
@@ -197,8 +242,5 @@ api_delete(Pid, Key) ->
         {Pid, delete_done} ->
             format("-> API: Deletion done!~n")
     end.
-
-% Zadatak 4: Napraviti proces koji prima osobu kao ključ i vraća listu prijatelja s određenim brojem skokova 
-% .. npr. za broj skokova 2 nađe i prijatelje od prijatelja. U listi se ne smije nalaziti više puta ista osoba.
 
 % Zadatak 5: Baciti iznimku ako je neki proces ubijen.
